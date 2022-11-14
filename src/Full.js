3dbd5b9e7f1cf507917016054544402cca5e073e
Ravula Pranay Kumar Reddy <rpranayreddy40@gmail.com>
2022-11-14 20:03:31 +0530

    Done with basic data of Resume Update

--- src/Full.js
+++ src/Full.js
@@ -5,9 +5,75 @@ import React from "react";
 class FullFun extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {Lname: "hi", githubLink: "", mailAdd: "", phoneNo: "", linkedinAc: ""
+    this.state = {
+      fullName: "Ravula Pranay Kumar Reddy",
+      link: {
+        github: "",
+        gmail: "rpranayreddy40@gmail.com",
+        whatsapp: "917680904589",
+        linkedin: "http://www.linkedin.com/in/rpranaykumarreddy"
+      },
+      education: [{
+        startYear: "2020",
+        endYear: "2024",
+        degree: "B.Tech in Computer Science and Engineering",
+        institution: "Indian Institute of Information Technology, Bhopal",
+        CGPA: "8.41"
+      }, {
+        startYear: "2018",
+        endYear: "2020",
+        degree: "Intermediate in MPC",
+        institution: "Nano Junior College, Nallakunta (TSBIE)",
+        percentage: "96.6"
+      }, {
+        startYear: "2020",
+        endYear: "2024",
+        degree: "B.Tech in Computer Science and Engineering",
+        institution: "Academic Heights Public School, Bhongir (CBSE)",
+        percentage: "87.2"
+      }],
+      portfolioLink: "https://pranay.wethinc.in",
+      projects: [{
+        category: "Chrome Extension",
+        name: "Youtube Playlist Ranger",
+        link: "https://chrome.google.com/webstore/detail/alpnapppdjbdbbaagjpchaaipphcnejo",
+        role: "Developer",
+        teamSize: "1",
+        duration: "4 days",
+        achievements: ["Understanding the architecture of Chrome Extensions.", "Constant maintenance & bug fixing with version controls."],
+        date: "October 22, 2022"
+      }, {
+        category: "Web App",
+        name: "Bulk Certificate Maker & Verifier",
+        link: "https://rpranaykumarreddy.github.io/youtube-Ext/",
+        role: "Developer",
+        teamSize: "1",
+        duration: "2 days",
+        achievements: ["Maker to produce certificate(.pdf) in bulk along with QR code for verification.", "Verifier to scan the QR Code on certificate & fetch the data of the certificate."],
+        date: "September 22, 2022"
+      }],
+      skills: [
+        ["Web Development", "HTML", "CSS", "JavaScript", "SQL", "ReactJS", "Django"],
+        ["Languages", "C lang"],
+        ["Soft Skills", "Problem Solving", "Effective Communication", "Management Skills", "Perceptive"],
+        ["Others", "Git", "GitHub", "Chrome Extensions", "Adobe Photoshop", "MS Access"]
+      ],
+      achievements: [
+        "Secured 2nd position in IBM Innovation Camp: AI | IBM & Edunet",
+        "Completion of Finding the leader in You (FLY)-Scholar Program | Competitiveness Mindset Institute"
+      ],
+      positions: [{
+        title: "Google Developer Student Club (GDSC) Lead",
+        timeline: "July 2022 - Present"
+      },
+      {
+        title: "Lead Developer at We Thinc Solutions (Freelance) ",
+        timeline: "December 2020 – June 2022",
+        achievements: ["Built a group to have an environment to work-on different Ideas & to increase efficiency."]
+      }
+      ]
     };
-    
+
   }
 
   handleChange = e => {
@@ -23,8 +89,9 @@ class FullFun extends React.Component {
 
   render() {
     return (
-    <>
-      <form>
+      <>
+        <div className='FormInter'>
+          <form>
             <input name="Lname" onChange={this.handleChange} value={this.state.Lname} type="text" placeholder="name" />
             <input name="githubLink" onChange={this.handleChange} value={this.state.githubLink} type="text" placeholder="githubLink" />
             <input name="mailAdd" onChange={this.handleChange} value={this.state.mailAdd} type="text" placeholder="mailAdd" />
@@ -32,9 +99,10 @@ class FullFun extends React.Component {
             <input name="linkedinAc" onChange={this.handleChange} value={this.state.linkedinAc} type="text" placeholder="linkedinAc" />
 
             <button onClick={this.handleSubmit}>Submit</button>
-        </form>
-      <Resume data={this.state} />
-    </>
+          </form>
+        </div>
+        <Resume data={this.state} />
+      </>
     );
   }
 }
--- src/Resume.js
+++ src/Resume.js
@@ -1,6 +1,202 @@
 import './resume.css';
+import imGithub from './github.svg';
+import imGmail from './gmail.svg';
+import imWhatsapp from './whatsapp.svg';
+import imLinkedin from './linkedin.svg';
+import imphone from './phone.svg';
 
-function Resume (props){
-    return (<p>{props.data.Lname}</p>);
+function ReIcon(props) {
+    const { img, lk, name } = props;
+    if (!(lk)) {
+        return null;
+    }
+    return (
+        <span className="ReLink">&nbsp;&nbsp;
+            <img src={img} />
+            &nbsp;
+            <a href={lk} target="_blank">{name}</a>
+            &nbsp;&nbsp;
+        </span>
+    );
+
+}
+function ReHeader(props) {
+    const { fullName, link } = props.ob;
+    return (<header className="teCen">
+        <h1 className="marBot">{fullName}</h1>
+        <p>
+            <ReIcon img={imGithub} lk={link.github} name="GitHub" />
+
+            <ReIcon img={imGmail} lk={link.gmail} name={link.gmail} />
+
+            <ReIcon img={imWhatsapp} lk={link.whatsapp} name={link.whatsapp} />
+
+            <ReIcon img={imLinkedin} lk={link.linkedin} name="LinkedIn" />
+        </p>
+        <br />
+    </header>
+    );
+}
+function Resume(props) {
+    const d = props.data;
+    return (<>
+        <div className="resume">
+            <ReHeader ob={d} />
+            <section>
+                <h2>Education</h2>
+                <table className="teCen">
+                    <tbody>
+                        <tr>
+                            <th className="noWrap">Year</th>
+                            <th>Degree</th>
+                            <th>Institution</th>
+                            <th>CGPA / Percentage</th>
+                        </tr>
+                        <tr>
+                            <td className="noWrap">2020-2024</td>
+                            <td>B.Tech (Computer Science and Engineering)</td>
+                            <td>Indian Institute of Information Technology, Bhopal</td>
+                            <td className="noWrap">CGPA: 8.41</td>
+                        </tr>
+                        <tr>
+                            <td className="noWrap">2018-2020</td>
+                            <td>Intermediate (TSBIE)</td>
+                            <td>Nano Junior College</td>
+                            <td className="noWrap">96.6%</td>
+                        </tr>
+                        <tr>
+                            <td className="noWrap">2018</td>
+                            <td>Class X (CBSE)</td>
+                            <td>Academic Heights School</td>
+                            <td className="noWrap">87.2%</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+            <br />
+            <section>
+                <h2>Projects</h2>
+                <article className="marBot">
+                    <h3>
+                        <b>
+                            <span>AI Project</span> | <span>Detection of Breast Cancer</span> |{" "}
+                            <span>
+                                <a href="http://" target="_blank" rel="noopener noreferrer">
+                                    Live Demo
+                                </a>
+                            </span>
+                        </b>
+                        <span className="proDate">February 2022</span>
+                    </h3>
+                    <h3>
+                        <b>
+                            <span>Team Leader &amp; Lead Developer</span>
+                        </b>{" "}
+                        | <span>Team size : 7 People</span>
+                    </h3>
+                    <ul>
+                        <li>Getting high accurate predictions for the health sector.</li>
+                        <li>Dynamic predictions based on datasets accuracy.</li>
+                    </ul>
+                </article>
+                <article className="marBot">
+                    <h3>
+                        <b>
+                            <span>AI Project</span> | <span>Detection of Breast Cancer</span> |{" "}
+                            <span>
+                                <a href="http://" target="_blank" rel="noopener noreferrer">
+                                    Live Demo
+                                </a>
+                            </span>
+                        </b>
+                        <span className="proDate">February 2022</span>
+                    </h3>
+                    <h3>
+                        <b>
+                            <span>Team Leader &amp; Lead Developer</span>
+                        </b>{" "}
+                        | <span>Team size : 7 People</span>
+                    </h3>
+                    <ul>
+                        <li>Getting high accurate predictions for the health sector.</li>
+                        <li>Dynamic predictions based on datasets accuracy.</li>
+                    </ul>
+                </article>
+                <article className="marBot">
+                    <h3>
+                        <b>
+                            <span>AI Project</span> | <span>Detection of Breast Cancer</span> |{" "}
+                            <span>
+                                <a href="http://" target="_blank" rel="noopener noreferrer">
+                                    Live Demo
+                                </a>
+                            </span>
+                        </b>
+                        <span className="proDate">February 2022</span>
+                    </h3>
+                    <h3>
+                        <b>
+                            <span>Team Leader &amp; Lead Developer</span>
+                        </b>{" "}
+                        | <span>Team size : 7 People</span>
+                    </h3>
+                    <ul>
+                        <li>Getting high accurate predictions for the health sector.</li>
+                        <li>Dynamic predictions based on datasets accuracy.</li>
+                    </ul>
+                </article>
+            </section>
+            <br />
+            <section>
+                <h2>Skills</h2>
+                <h3>
+                    <b>Language: </b>C Language
+                </h3>
+                <h3>
+                    <b>Web Development: </b>HTML, CSS, JavaScript, SQL, ReactJS, Django
+                </h3>
+                <h3>
+                    <b>Other: </b>Photo Shop, MS Access
+                </h3>
+                <h3>
+                    <b>Soft Skills: </b>Problem Solving, Effective Communication, Management
+                    Skills, Perceptive
+                </h3>
+            </section>
+            <br />
+            <section>
+                <h2>Achiecements</h2>
+                <ul>
+                    <li>
+                        Secured 2nd position in IBM Innovation Camp: AI | IBM &amp; Edunet
+                    </li>
+                    <li>
+                        Completion of Finding the leader in You (FLY)-Scholar Program |
+                        Competitiveness Mindset Institute
+                    </li>
+                </ul>
+            </section>
+            <br />
+            <section>
+                <h2>Positions of Responsibilities</h2>
+                <h3>
+                    <b>Google Developer Student Club (GDSC) Lead</b>
+                    <span className="proDate">July 2022 - Present</span>
+                </h3>
+                <br />
+                <h3>
+                    <b>Lead Developer at We Thinc Solutions</b>
+                    <span className="proDate">December 2022 - Present</span>
+                </h3>
+                <ul>
+                    <li>
+                        {" "}
+                        Built a group to have an environment to work-on different Ideas &amp; to
+                        increase efficiency.
+                    </li>
+                </ul>
+            </section></div>
+    </>
+    );
 }
 export default Resume;
